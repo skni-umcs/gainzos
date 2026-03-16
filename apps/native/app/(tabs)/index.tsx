85bf6969fb8c93be0b2326ef2fa2a3f26a777f12
Damian Kawerski <damian.kawerski@gmail.com>
2026-03-16 22:31:37 +0100

    kolorki

--- apps/native/app/(tabs)/index.tsx
+++ apps/native/app/(tabs)/index.tsx
@@ -1,12 +1,18 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { QuoteCard } from '@/components/feats/home/quote-card';
 import { ScreenTitle } from '@/components/ui/screen-title';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useTranslation } from 'react-i18next';
 
 export default function Index() {
+  const { t } = useTranslation();
+
   return (
-    <View className="flex-1 items-center justify-center px-6">
-      <ScreenTitle title="Welcome back" />
-      <QuoteCard quote="Nie ma nic lepszego niż być sobą." />
-    </View>
+    <SafeAreaView className="flex-1">
+      <View className="flex-1  px-6 ">
+        <ScreenTitle title={t('welcomeBack')} />
+        <QuoteCard quote={t('quote')} />
+      </View>
+    </SafeAreaView>
   );
 }
