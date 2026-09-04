dc34c2750f2cd01b433448e9761a6d17950477f7
lena.kiselova <adres@email.com>
2026-09-04 12:38:01 +0200

    dashboard changes add/delete functions

--- apps/server/src/main/java/com/gainzos/server/mappers/ExercisesMapper.java
+++ apps/server/src/main/java/com/gainzos/server/mappers/ExercisesMapper.java
@@ -8,10 +8,10 @@ import com.gainzos.server.entities.Exercise;
 
 @Mapper(componentModel = "spring", uses = {MediaMapper.class, ExercisesTypeMapper.class})
 public interface ExercisesMapper {
-    @Mapping(source = "exerciseType", target = "exercisesType")
+    @Mapping(source = "exerciseType", target = "exercisesType", qualifiedByName = "toDTO")
     ExerciseDTO toDTO(Exercise exercise);
 
-    @Mapping(source = "exercisesType", target = "exerciseType")
+    @Mapping(source = "exercisesType", target = "exerciseType", qualifiedByName = "toEntity")
     Exercise toEntity(ExerciseDTO exerciseDTO);
 
     @Mapping(target = "image", ignore = true)
